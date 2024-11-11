4d4a5015d682d410e3f1c0fa199ef604c6679336
AmmarQt <craetive7@gmail.com>
2024-11-11 19:45:33 +0300

    task 2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,31 @@ import "./App.css";
 import storedimage from "./image.png";
 
 function App() {
+  const products = [
+    {
+      name: "cookie one",
+      price: "2 KWD",
+      image:
+        "https://cdn.craft.cloud/224393fa-1975-4d80-9067-ada3cb5948ca/assets/bananacookies_detail_1-0002_2022.jpg",
+    },
+    {
+      name: "cookie two",
+      price: "3 KWD",
+      image:
+        "https://cdn.craft.cloud/224393fa-1975-4d80-9067-ada3cb5948ca/assets/bananacookies_detail_1-0002_2022.jpg",
+    },
+  ];
+
+  const productslist = products.map((product, index) => {
+    return (
+      <div className="dev-item">
+        <img className="image-style" src={product.image}></img>
+        <p>{product.name}</p>
+        <p>{product.price}</p>
+      </div>
+    );
+  });
+
   return (
     <div className="App">
       <h1 className="App-header">
@@ -12,6 +37,7 @@ function App() {
         <strong>where cookies maniace gathers</strong>
       </p>
       <img className="Stored-Image" src={storedimage}></img>
+      <div className="dev-container">{productslist}</div>
     </div>
   );
 }
